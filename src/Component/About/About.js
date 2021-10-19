1efad49f4f660aaf7607c753e2da2af81ebe4584
rdrayhan7916 <rdrayhan7916@gmail.com>
2021-10-19 21:18:41 +0600

    about

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about</h2>
+        <div className="row">
+            <div className="col-md-6">
+                <img className="p-5 img-fluid" src="https://media.istockphoto.com/photos/nurse-visiting-a-senior-man-patient-at-home-picture-id1280119548?k=20&m=1280119548&s=612x612&w=0&h=lMdzCiTl1EZCX5VRuRWbTDbsahMEK6NgHyR-hCYeyq4=" alt="" />
+            </div>
+            <div className="col-md-6">
+                <h6 className="text-primary">About Us</h6>
+                <h1>We Are Your Trusted Friend</h1>
+                <p>Medizo is a trusted name of Medical Services who is always at your side and your health is our first priority.</p>
+                <p>Medizo Care will be administered through plan-based customizable programs that incorporate partnership between family members and the care givers for long term illness or disease management.</p>
+                <h5>24/7 Support</h5>
+                <p>Our Medical team of different department for long term illness writer and editors makes all the </p>
+                <h5>Emergency Support </h5>
+                <p>Our Medical team of different department for long term illness writer and editors makes all the</p>
+                <button className="btn btn-primary m-4">Explore More</button>
+            </div>
         </div>
     );
 };
